03ed0a83b81b277ef2e1669816699baa9db9baf8
Andrew Larned <andrew.larned@gmail.com>
2017-10-06 08:33:29 -0400

    Unit tests pass

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "indent": ["error", 4],
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+    },
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
+/* global it, expect */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
+Enzyme.configure({ adapter: new Adapter() });
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+    const component = shallow(<App />);
+    expect(component.exists()).toEqual(true);
 });
