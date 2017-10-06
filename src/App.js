d81ba49de99229559fb6d5b5ba1f65c8c32313e8
Andrew Larned <andrew.larned@gmail.com>
2017-10-06 11:20:26 -0400

    Store built and connected

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import './App.css';
+import logo from './logo.svg';
 import AddTodo from './components/AddTodo';
+import actions from './actions';
 
-const App = () => (
+export const App = ({ submitTodo }) => (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to React</h1>
       </header>
       <p className="App-intro">
-        <AddTodo submitTodo={() => {}} />
+        <AddTodo submitTodo={submitTodo} />
       </p>
     </div>
 );
 
-export default App;
+App.propTypes = {
+    submitTodo: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => state.todoListApp;
+
+const mapDispatchToProps = dispatch => ({
+    submitTodo: (text) => {
+        if (text) {
+            dispatch(actions.submitTodo(text));
+        }
+    },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,16 @@
-/* global it, expect */
+/* global it, expect, jest */
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import App from './App';
+import { App } from './App';
+import { initialState } from './reducers/';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
-    const component = shallow(<App />);
+    const mockFunction = jest.fn();
+
+    const component = shallow(<App state={initialState} submitTodo={mockFunction} />);
     expect(component.exists()).toEqual(true);
 });
